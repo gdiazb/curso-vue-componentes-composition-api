e18bfa8344b854dcf65c40eb91057ad7c72c47aa
Geraldine Diaz <gdiaz.contact@gmail.com>
2022-11-06 18:03:32 -0500

    using stup with composition api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <Home></Home>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from "@/components/Home.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Home,
   },
 };
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hola composition Api</div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+export default {
+  setup() {
+    onMounted(() => {
+      console.log("mounted");
+    });
+  },
+};
+</script>
