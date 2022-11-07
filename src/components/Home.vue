83c075bc4b040ca837113d08a6fbde85496d9d93
Geraldine Diaz <geraldine.diaz@mercadolibre.com.co>
2022-11-07 07:17:27 -0500

    using watch with composition api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref, reactive, watch } from "vue";
 export default {
   setup() {
     const text = ref("gerli");
@@ -11,6 +11,12 @@ export default {
     const objeto = reactive({ contador: 0 });
 
     setInterval(() => objeto.contador++, 500);
+
+    //watch
+    watch(objeto, (valor, anterior) => {
+      console.log("valor:", valor);
+      console.log("anterior:", anterior);
+    });
 
     return {
       text,
